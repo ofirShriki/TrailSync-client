07ab7377fc1ac5ec89f91a67b065383f97db0e9e
TamarTal23 <212927487@idf.il>
2026-01-31 13:53:05 +0200

    ✨ feat: implement GenericModal component and integrate into PostModal and EditProfileModal

---
+++ src/components/EditProfileModal/EditProfileModal.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import GenericModal from '../GenericModal/GenericModal';
+import type { User } from '../../types/user';
+
+interface EditProfileModalProps {
+  isModalOpen: boolean;
+  setIsModalOpen: (isOpen: boolean) => void;
+  user: User;
+}
+
+const EditProfileModal: React.FC<EditProfileModalProps> = ({ isModalOpen, setIsModalOpen }) => {
+  return (
+    <GenericModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}>
+      dsad
+    </GenericModal>
+  );
+};
+
+export default EditProfileModal;
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,51 +1,67 @@
-import type React from 'react';
-import Box from '@mui/material/Box';
-import { Avatar, Divider, IconButton, Typography } from '@mui/material';
-import PostList from '../PostList/PostList';
-import { users } from '../../constants/staticInfo';
-import { Edit } from '@mui/icons-material';
+import type React from "react";
+import Box from "@mui/material/Box";
+import { Avatar, Divider, IconButton, Typography } from "@mui/material";
+import PostList from "../PostList/PostList";
+import { users } from "../../constants/staticInfo";
+import { Edit } from "@mui/icons-material";
+import { useState } from "react";
+import EditProfileModal from "../EditProfileModal/EditProfileModal";
 
 const Profile: React.FC = () => {
-  const { username, profilePicture, posts } = users[0];
+	const user = users[0];
+	const { username, profilePicture, posts } = user;
+	const [isEditModalOpen, setIsEditModalOpen] = useState(false);
 
-  return (
-    <Box>
-      <Box
-        sx={{
-          display: 'flex',
-          gap: 4,
-          alignItems: 'flex-start',
-          p: 2,
-        }}
-      >
-        <Avatar src={profilePicture} alt={username} sx={{ width: 100, height: 100 }} />
-        <Box sx={{ flex: 1 }}>
-          <Box
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              mb: 1,
-              mt: 0.5,
-            }}
-          >
-            <Typography variant="h5" fontWeight={500}>
-              {username}
-            </Typography>
+	return (
+		<Box>
+			<Box
+				sx={{
+					display: "flex",
+					gap: 4,
+					alignItems: "flex-start",
+					p: 2,
+				}}
+			>
+				<Avatar
+					src={profilePicture}
+					alt={username}
+					sx={{ width: 100, height: 100 }}
+				/>
+				<Box sx={{ flex: 1 }}>
+					<Box
+						sx={{
+							display: "flex",
+							alignItems: "center",
+							mb: 1,
+							mt: 0.5,
+						}}
+					>
+						<Typography variant="h5" fontWeight={500}>
+							{username}
+						</Typography>
 
-            <IconButton color="primary">
-              <Edit />
-            </IconButton>
-          </Box>
+						<IconButton
+							color="primary"
+							onClick={() => setIsEditModalOpen(true)}
+						>
+							<Edit />
+						</IconButton>
+					</Box>
 
-          <Typography variant="body1" sx={{ mb: 1, gap: 0.5 }}>
-            <strong>{posts?.length}</strong> posts
-          </Typography>
-        </Box>
-      </Box>
-      <Divider sx={{ mt: 2 }} />
-      {posts && <PostList posts={posts} />}
-    </Box>
-  );
+					<Typography variant="body1" sx={{ mb: 1, gap: 0.5 }}>
+						<strong>{posts?.length}</strong> posts
+					</Typography>
+				</Box>
+			</Box>
+			<Divider sx={{ mt: 2 }} />
+			{posts && <PostList posts={posts} />}
+			<EditProfileModal
+				isModalOpen={isEditModalOpen}
+				setIsModalOpen={setIsEditModalOpen}
+				user={user}
+			/>
+		</Box>
+	);
 };
 
 export default Profile;
