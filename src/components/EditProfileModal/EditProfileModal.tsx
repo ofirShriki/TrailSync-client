9bc8bb63fd0d9c0e76bd242db9687d7a59372515
TamarTal23 <212927487@idf.il>
2026-01-31 13:59:02 +0200

    pr fixes

--- src/components/EditProfileModal/EditProfileModal.tsx
+++ src/components/EditProfileModal/EditProfileModal.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import GenericModal from '../GenericModal/GenericModal';
-import type { User } from '../../types/user';
+import React from "react";
+import GenericModal from "../GenericModal/GenericModal";
+import type { User } from "../../types/user";
 
 interface EditProfileModalProps {
-  isModalOpen: boolean;
-  setIsModalOpen: (isOpen: boolean) => void;
-  user: User;
+	isModalOpen: boolean;
+	setIsModalOpen: (isOpen: boolean) => void;
+	user: User;
 }
 
-const EditProfileModal: React.FC<EditProfileModalProps> = ({ isModalOpen, setIsModalOpen }) => {
-  return (
-    <GenericModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}>
-      dsad
-    </GenericModal>
-  );
+const EditProfileModal: React.FC<EditProfileModalProps> = ({
+	isModalOpen,
+	setIsModalOpen,
+}) => {
+	return (
+		<GenericModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}>
+			TODO: add edit profile form here
+		</GenericModal>
+	);
 };
 
 export default EditProfileModal;
