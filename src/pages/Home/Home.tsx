2511624e1e76a0c43b0bba69649bb3bb64fe35bc
TamarTal23 <212927487@idf.il>
2026-02-17 20:13:42 +0200

    pr fixes

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -10,64 +10,63 @@ import { postService } from "../../services/postService";
 import styles from "./Home.styles";
 
 const Home: React.FC = () => {
-	const [isHovered, setIsHovered] = useState(false);
-	const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+  const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
 
-	const { data, isLoading, fetchNextPage, hasNextPage, isFetchingNextPage } =
-		useInfiniteQuery({
-			queryKey: [QUERY_KEYS.POSTS],
-			queryFn: async ({ pageParam = 1 }) =>
-				await postService.getAllPosts(
-					undefined,
-					pageParam,
-					Number(import.meta.env.VITE_BATCH_SIZE),
-				),
-			getNextPageParam: (lastPage, allPages) =>
-				lastPage.hasMore ? allPages.length + 1 : undefined,
-			initialPageParam: 1,
-			refetchOnReconnect: false,
-			refetchOnWindowFocus: false,
-		});
+  const { data, isLoading, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useInfiniteQuery({
+      queryKey: [QUERY_KEYS.POSTS],
+      queryFn: async ({ pageParam = 1 }) =>
+        await postService.getAllPosts({
+          page: pageParam,
+          batchSize: Number(import.meta.env.VITE_BATCH_SIZE),
+        }),
+      getNextPageParam: (lastPage, allPages) =>
+        lastPage.hasMore ? allPages.length + 1 : undefined,
+      initialPageParam: 1,
+      refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
+    });
 
-	const posts = useMemo(
-		() => data?.pages.flatMap(page => page.data) ?? [],
-		[data],
-	);
+  const posts = useMemo(
+    () => data?.pages.flatMap(page => page.data) ?? [],
+    [data]
+  );
 
-	const handleCreatePost = () => {
-		setIsCreateModalOpen(true);
-	};
+  const handleCreatePost = () => {
+    setIsCreateModalOpen(true);
+  };
 
-	return (
-		<Box sx={styles.root}>
-			<PostList
-				posts={posts}
-				isLoading={isLoading}
-				onLoadMore={fetchNextPage}
-				hasMore={hasNextPage}
-				isFetchingMore={isFetchingNextPage}
-			/>
-			<Fab
-				color="primary"
-				onClick={handleCreatePost}
-				onMouseEnter={() => setIsHovered(true)}
-				onMouseLeave={() => setIsHovered(false)}
-				variant={isHovered ? "extended" : "circular"}
-				sx={styles.createButton}
-			>
-				<AddIcon />
-				{isHovered && (
-					<Typography variant="button">Share your trail </Typography>
-				)}
-			</Fab>
-			{isCreateModalOpen && (
-				<CreatePostModal
-					isModalOpen={isCreateModalOpen}
-					setIsModalOpen={setIsCreateModalOpen}
-				/>
-			)}
-		</Box>
-	);
+  return (
+    <Box sx={styles.root}>
+      <PostList
+        posts={posts}
+        isLoading={isLoading}
+        onLoadMore={fetchNextPage}
+        hasMore={hasNextPage}
+        isFetchingMore={isFetchingNextPage}
+      />
+      <Fab
+        color="primary"
+        onClick={handleCreatePost}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        variant={isHovered ? "extended" : "circular"}
+        sx={styles.createButton}
+      >
+        <AddIcon />
+        {isHovered && (
+          <Typography variant="button">Share your trail </Typography>
+        )}
+      </Fab>
+      {isCreateModalOpen && (
+        <CreatePostModal
+          isModalOpen={isCreateModalOpen}
+          setIsModalOpen={setIsCreateModalOpen}
+        />
+      )}
+    </Box>
+  );
 };
 
 export default Home;
